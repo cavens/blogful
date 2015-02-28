87c3b9b8d581f0ce766a18389e6f0a6314a56ba3
cavens <jef@cavens.io>
2015-02-28 10:05:54 +0000

    initial commit

--- manage.py
+++ manage.py
@@ -2,8 +2,10 @@ import logging
 logging.basicConfig(filename="blog.log", level=logging.DEBUG)
 import os
 from flask.ext.script import Manager
-
 from blog import app
+from getpass import getpass
+from werkzeug.security import generate_password_hash
+from blog.models import User
 
 manager = Manager(app)
 
@@ -24,6 +26,25 @@ def seed():
     session.add(post)
     logging.debug(i)
   session.commit()
+  
+@manager.command
+def adduser():
+  name = raw_input("Name:")
+  email = raw_input("Email:")
+  if session.query(User).filter(User.email = email).first():
+    print "User with that email address already exists"
+    return
+  
+  password =""
+  password_2 =""
+  while not (password and password_2) or password != password_2:
+    password=getpass("Password:")
+    password_2=getpass("Re-enter password:")
+  user = User(name=name,email=email,password=generate_password_hash(password))
+  session.add(user) 
+  session.commit()
+  
+  
   
   
 if __name__ == "__main__":
